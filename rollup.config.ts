f520c8557a8793ddc1a388bc110e18225cc23183
Raiondesu <a.raiondesu@gmail.com>
2018-10-09 13:14:04 +0300

    Initial template

---
+++ rollup.config.ts
@@ -0,0 +1,52 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
+// import pkg from './package.json';
+
+// const libraryName = pkg.name;
+
+const input = `src/index.ts`;
+const output = format => `dist/web-orm.${format}.js`;
+const common = target => ({
+  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+  external: [],
+  watch: {
+    include: 'src/**',
+  },
+  plugins: [
+    // Allow json resolution
+    json(),
+    // Compile TypeScript files
+    typescript({ useTsconfigDeclarationDir: true, tsconfigOverride: { compilerOptions: {
+      target
+    } } }),
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
+
+    // Resolve source maps to the original source
+    sourceMaps(),
+  ],
+});
+
+export default [
+  {
+    input,
+    output: [
+      { file: output('cjs'), format: 'cjs', sourcemap: true, exports: 'named' },
+      { file: output('umd'), format: 'umd', sourcemap: true, name: 'web-orm', exports: 'named' },
+      { file: output('iife'), format: 'iife', sourcemap: true, name: 'web-orm', exports: 'named' },
+    ],
+    ...common('es5')
+  },
+  {
+    input,
+    output: { file: 'dist/web-orm.es.js', format: 'es', sourcemap: true },
+    ...common('es6')
+  }
+];
