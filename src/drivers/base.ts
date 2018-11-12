484fe11c8716f15e6f1856e369a56a1f5d4ebaa4
Raiondesu <a.raiondesu@gmail.com>
2018-11-12 17:02:05 +0300

    Add todo for drivers

--- src/drivers/base.ts
+++ src/drivers/base.ts
@@ -12,27 +12,30 @@ export abstract class Driver {
     protected connection: Connection
   ) {}
 
-  public abstract create<A extends object, R extends IRepoData = IRepoData>(
+  /* TODO: additional driver functionality?.. */
+
+
+  public abstract create<A, R extends IRepoData = IRepoData>(
     repository: R,
     data: A
   ): Promise<A>;
 
-  public abstract read<A extends object, R extends IRepoData = IRepoData>(
+  public abstract read<A, R extends IRepoData = IRepoData>(
     repository: R,
     id: any
   ): Promise<A>;
 
-  public abstract update<A extends object, R extends IRepoData = IRepoData>(
+  public abstract update<A, R extends IRepoData = IRepoData>(
     repository: R,
     id: any,
     query: (data: A) => Partial<A>
   ): Promise<A>;
-  public abstract update<A extends object, R extends IRepoData = IRepoData>(
+  public abstract update<A, R extends IRepoData = IRepoData>(
     repository: R,
     data: Partial<A>
   ): Promise<A>;
 
-  public abstract delete<A extends object, R extends IRepoData = IRepoData>(
+  public abstract delete<A, R extends IRepoData = IRepoData>(
     repository: R,
     id: any
   ): Promise<A>;
