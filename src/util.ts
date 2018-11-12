39d2b10af37c519f3ca60e134c2d03b06e5977c8
Raiondesu <a.raiondesu@gmail.com>
2018-11-12 07:08:49 +0300

    Fix link in utils

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,7 @@
  * fromPath
  * Returns a value from an object by a given path (usually string).
  *
- * https://gist.github.com/Raiondesu/759425dede5b7ff38db51ea5a1fb8f11
+ * @see [gist](https://gist.github.com/Raiondesu/759425dede5b7ff38db51ea5a1fb8f11)
  *
  * @param obj an object to get a value from.
  * @param path to get a value by.
