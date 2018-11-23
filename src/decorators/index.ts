6f12c50c78dc22f17710579b427903ca80b9e09f
Raiondesu <a.raiondesu@gmail.com>
2018-11-23 20:19:01 +0300

    Fix enumerable decorator

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,12 +1,7 @@
 export const enumerable = (isEnumerable: boolean = true) => function (target: object, key: string, desc?: PropertyDescriptor) {
-  if (desc) {
-    desc.enumerable = isEnumerable;
-  } else {
-    Reflect.deleteProperty(target, key);
-    Reflect.defineProperty(target, key, {
-      value: undefined,
-      enumerable: isEnumerable,
-      writable: true
-    });
+  let descriptor = Object.getOwnPropertyDescriptor(target, key) || {};
+  if (descriptor.enumerable != isEnumerable) {
+    descriptor.enumerable = isEnumerable;
+    Object.defineProperty(target, key, descriptor);
   }
 };
