cd7dc553dc6b565342b04d441b8a3b7d6f050359
Raiondesu <a.raiondesu@gmail.com>
2018-11-27 12:03:07 +0300

    Fix decorator types

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,4 +1,4 @@
-export const enumerable = (isEnumerable: boolean = true) => function (target: object, key: string, desc?: PropertyDescriptor) {
+export const enumerable = (isEnumerable: boolean = true) => function (target: object, key: string, desc?: any) {
   let descriptor = Object.getOwnPropertyDescriptor(target, key) || {};
   if (descriptor.enumerable != isEnumerable) {
     descriptor.enumerable = isEnumerable;
